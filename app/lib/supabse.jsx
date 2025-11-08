c8958a43ac57e27247867f8af9348ca209c2dda0
tijesuni124 <tijesuniadelowo@gmail.com>
2025-11-08 02:43:46 +0100

    Update Supabase URL and anon key
    Updated Supabase configuration with project-specific URL and key.

--- app/lib/supabse.jsx
+++ app/lib/supabse.jsx
@@ -1,16 +1,18 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-// replace with your values
-const SUPABASE_URL = "https://YOUR_PROJECT_URL.supabase.co";
-const SUPABASE_KEY = "YOUR_ANON_PUBLIC_KEY";
+// Use your project URL and anon key here
+const SUPABASE_URL = "https://wpaomomrescefzjojzpb.supabase.co";
+const SUPABASE_KEY = "sb_secret_DLkc1vDWzMn2vTwlwTHRnA_BycYbbKn"; // normally store in env
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false, // ✅ prevents "window is not defined" in React Native
+    detectSessionInUrl: false, // ✅ fixes "window is not defined"
   },
 });
+
+
 
